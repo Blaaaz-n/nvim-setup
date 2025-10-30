b324a84aef81d6773601a726b17d6c33bdf58626
ahmed <ahmedbelm51@gmail.com>
2025-10-30 00:48:44 -0400

    Fix Error

--- lua/ahmed/packer.lua
+++ lua/ahmed/packer.lua
@@ -21,27 +21,4 @@ require("lazy").setup({
       vim.cmd("colorscheme rose-pine")
     end,
   },
-})cal lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      vim.cmd("colorscheme rose-pine")
-    end,
-  },
 })
